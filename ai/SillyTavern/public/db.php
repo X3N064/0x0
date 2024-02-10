417f8869fa2f2462930fd89b1c349caa0ab9853d
Ken <86743486+X3N064@users.noreply.github.com>
2024-02-10 23:11:39 +0900

    Update db.php

--- ai/SillyTavern/public/db.php
+++ ai/SillyTavern/public/db.php
@@ -1,12 +1,18 @@
 <?php
-$servername = "localhost";
+
+$host = "localhost";
+$dbname = "creds";
 $username = "root";
 $password = "Tlagus47880112++**";
-$dbname = "creds";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+$mysqli = new mysqli(hostname: $host,
+                     username: $username,
+                     password: $password,
+                     database: $dbname);
+
+if ($mysqli->connect_errno) {
+    die("Connection error: " . $mysqli->connect_error);
 }
-?>
+
+return $mysqli;
