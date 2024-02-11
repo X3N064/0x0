896ef300fdf8b990486355fe676e795c4cbb3816
Hyun Sim <kitoko148@gmail.com>
2024-02-10 23:00:34 -0800

    db.php mod

--- html/db.php
+++ html/db.php
@@ -1,15 +1,21 @@
 <?php
 
-$host = "3.38.79.93";
-$port = 58923;
+$host = "localhost";
 $dbname = "creds";
-$username = "root";
+$port = 3306;
+$username = "admin";
 $password = "Qwer1234**";
 
+//Create a new MySQLi object for database connection
 $conn = new mysqli($host, $username, $password, $dbname, $port);
 
-// Check connection
+// Check if the connection was successful
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    // If connection failed, print an error message and terminate script
+        die("Connection failed: " . $conn->connect_error);
 }
+
+// If connection succeeded, print a success message
+// echo "DB connected";
 ?>
+
